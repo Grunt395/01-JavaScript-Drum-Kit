efb3aa65f0325f949a67702f08ddf219bfff258b
Grunt395 <grantlew@att.net>
2024-04-02 13:36:52 -0700

    Added transitionend event

--- script.js
+++ script.js
@@ -3,13 +3,24 @@ window.addEventListener("keydown", function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-    console.log(audio);
+    // console.log(e);
+    // console.log(audio);
+
     if (!audio) {
         return; // Stops the function from running on invalid key press
     }
     audio.currentTime = 0; // Rewinds audio to the start
     audio.play(); // Plays the audio element
 
-    key.classList.add("playing");
+    key.classList.add("playing"); // Adds class "playing" to a key
+
+    function removeTransition(e) {
+        console.log(e);
+    }
+
+    const keys = document.querySelectorAll(".key"); // Select all keys
+
+    keys.forEach(key => key.addEventListener("transitionend", removeTransition)); 
+    // Each key has an event listener, transitionend, added to it, and when a transition is endeing, we will remove it.
 
 });
