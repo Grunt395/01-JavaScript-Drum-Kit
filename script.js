15933bc7a001b79431f8533ddf029d716c2236a8
Grunt395 <grantlew@att.net>
2024-04-02 13:24:42 -0700

    Added audio player & playing class

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 window.addEventListener("keydown", function(e) {
     
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    console.log(audio);
+    if (!audio) {
+        return; // Stops the function from running on invalid key press
+    }
+    audio.currentTime = 0; // Rewinds audio to the start
+    audio.play(); // Plays the audio element
+
+    key.classList.add("playing");
+
 });
